7f6f218a41b0e13324be8cd3dc1b21dc260e1c88
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-09 14:16:15 -0500

    Created show 1 route, action and view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,17 @@
 class PicturesController < ApplicationController
 
+	before_filter :get_picture, only: [:show]
+
 	def index
 		@pictures = Picture.all
+	end
+
+	def show
+	end
+
+	private
+
+	def get_picture
+		@picture = Picture.find(params[:id])
 	end
 end
